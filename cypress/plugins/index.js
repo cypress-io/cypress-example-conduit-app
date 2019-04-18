5581e4f4a20bbb7473babe2ac26a48c8e4f25de1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-18 08:10:20 -0400

    clean up error handling

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,43 +1,31 @@
-const { join } = require("path");
-const knexFactory = require("knex");
+const { join } = require('path')
+const knexFactory = require('knex')
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on("task", {
-    deleteAllArticles() {
+  on('task', {
+    deleteAllArticles () {
+      const filename = join(__dirname, '..', '..', 'server', '.tmp.db')
       const knex = knexFactory({
-        client: "sqlite3",
+        client: 'sqlite3',
         connection: {
-          filename: join(__dirname, "..", "..", "server", ".tmp.db")
+          filename
         },
         useNullAsDefault: true
-      });
+      })
+      // if we are trying to truncate a non-existing table
+      // that is ok - the server API will create them
+      const onError = err =>
+        err.toString().includes('no such table') ? null : Promise.reject(err)
+
+      // truncates all tables created by previous tests
+      // or manually by using the application
       return Promise.all([
-        knex
-          .truncate("Articles")
-          .catch(err =>
-            err.toString().includes("no such table")
-              ? undefined
-              : Promise.reject(err)
-          ),
-        knex
-          .truncate("ArticleTags")
-          .catch(err =>
-            err.toString().includes("no such table")
-              ? undefined
-              : Promise.reject(err)
-          ),
-        ,
-        knex
-          .truncate("Comments")
-          .catch(err =>
-            err.toString().includes("no such table")
-              ? undefined
-              : Promise.reject(err)
-          )
-      ]);
-    },
-    registerNewUserIfNeeded() {}
-  });
-};
+        knex.truncate('Articles').catch(onError),
+        knex.truncate('ArticleTags').catch(onError),
+        knex.truncate('Comments').catch(onError)
+      ])
+    }
+  })
+}

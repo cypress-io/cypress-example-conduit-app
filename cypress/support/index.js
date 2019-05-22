ec461fa6d4ae57083458f505002b75cb70d999ba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-22 17:22:56 -0400

    working

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,15 +1,16 @@
 import '@applitools/eyes-cypress/commands'
+import 'cypress-istanbul/support'
 
 // a custom Cypress command to login using XHR call
 // and then set the received token in the local storage
 
 Cypress.Commands.add('login', () => {
   cy.request('POST', 'http://localhost:3000/api/users/login', {
-    user: Cypress.env('user'),
+    user: Cypress.env('user')
   })
     .its('body.user.token')
     .should('exist')
-    .then((token) => {
+    .then(token => {
       localStorage.setItem('jwt', token)
       // with this token set, when we visit the page
       // the web application will have the user logged in
@@ -30,9 +31,9 @@ Cypress.Commands.add('registerUserIfNeeded', () => {
         username: 'testuser',
         image: 'https://robohash.org/6FJ.png?set=set3&size=150x150',
         // email, password
-        ...Cypress.env('user'),
-      },
+        ...Cypress.env('user')
+      }
     },
-    failOnStatusCode: false,
+    failOnStatusCode: false
   })
 })

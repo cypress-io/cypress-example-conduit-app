42fd46485471216890131fd6e85f566d3f5717bf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-25 17:11:24 -0400

    prevent race

--- cypress/integration/profile-spec.js
+++ cypress/integration/profile-spec.js
@@ -20,9 +20,11 @@ describe('Profile', () => {
       .clear()
       .type('my new bio')
     cy.get('form').submit()
+    cy.location('pathname').should('equal', '/')
 
     // saved bio should be displayed
     cy.get('[data-cy=profile]').click()
+    cy.location('pathname').should('equal', '/@testuser')
     cy.get('[data-cy=user-info]')
       .should('be.visible')
       .find('[data-cy=bio]')

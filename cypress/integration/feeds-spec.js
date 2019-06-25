c69fe87431fbad19f10ad5a54a202cf5a4166863
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-25 14:28:33 -0400

    feed spec

--- cypress/integration/feeds-spec.js
+++ cypress/integration/feeds-spec.js
@@ -3,20 +3,15 @@
 describe('Conduit', () => {
   before(() => cy.registerUserIfNeeded())
   beforeEach(() => {
-    cy.request('POST', 'http://localhost:3000/api/users/login', {
-      user: Cypress.env('user')
-    })
-      .its('body.user.token')
-      .should('exist')
-      .then(token => {
-        localStorage.setItem('jwt', token)
-      })
-
-    cy.visit('/')
+    cy.task('deleteAllArticles')
+    cy.login()
   })
 
   it('shows feeds', () => {
     cy.contains('a.nav-link', 'Your Feed').should('have.class', 'active')
-    cy.contains('a.nav-link', 'Global Feed').should('not.have.class', 'active')
+    cy.contains('a.nav-link', 'Global Feed')
+      .should('not.have.class', 'active')
+      .click()
+      .should('have.class', 'active')
   })
 })

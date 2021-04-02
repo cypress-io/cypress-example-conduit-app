498731260b02065d0e99f7f23165d98751a70640
Gleb Bahmutov <bahmutov@yahoo.com>
2021-04-02 13:43:14 -0400

    Bump code coverage plugin to v3 (#316)
    * update circleci url
    
    * bump code coverage plugin to v3

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,70 +6,74 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 
   // tasks for code coverage
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config)
 
   // tasks for resetting database during tests
   on('task', {
-    cleanDatabase () {
+    cleanDatabase() {
       const filename = join(__dirname, '..', '..', 'server', '.tmp.db')
       const knex = knexFactory({
         client: 'sqlite3',
         connection: {
-          filename
+          filename,
         },
-        useNullAsDefault: true
+        useNullAsDefault: true,
       })
       // if we are trying to truncate a non-existing table
       // that is ok - the server API will create them
-      const onError = err =>
+      const onError = (err) =>
         err.toString().includes('no such table') ? null : Promise.reject(err)
 
       // truncates all tables which removes data left by previous tests
       return Promise.all([
         knex
           .truncate('Users')
-          .catch(err =>
+          .catch((err) =>
             err.toString().includes('no such table')
               ? undefined
               : Promise.reject(err)
           ),
         knex
           .truncate('Articles')
-          .catch(err =>
+          .catch((err) =>
             err.toString().includes('no such table')
               ? undefined
               : Promise.reject(err)
           ),
         knex
           .truncate('ArticleTags')
-          .catch(err =>
+          .catch((err) =>
             err.toString().includes('no such table')
               ? undefined
               : Promise.reject(err)
           ),
         knex
           .truncate('Comments')
-          .catch(err =>
+          .catch((err) =>
             err.toString().includes('no such table')
               ? undefined
               : Promise.reject(err)
           ),
         knex
           .truncate('Followers')
-          .catch(err =>
+          .catch((err) =>
             err.toString().includes('no such table')
               ? undefined
               : Promise.reject(err)
           ),
         knex
           .truncate('ArticleFavorites')
-          .catch(err =>
+          .catch((err) =>
             err.toString().includes('no such table')
               ? undefined
               : Promise.reject(err)
-          )
+          ),
       ])
     },
-    registerNewUserIfNeeded () {}
+    registerNewUserIfNeeded() {},
   })
+
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
 }
